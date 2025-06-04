231ce6133fcecf2d300a843dd6020eb5b6a07115
Şeyma Ertuğrul <seymaertgrl@gmail.com>
2025-06-04 17:20:59 +0300

    SeymaErtugrul/Seyma/JA-1-Enums

--- src/main/java/org/example/Enums/Gun.java
+++ src/main/java/org/example/Enums/Gun.java
@@ -1,4 +1,5 @@
 package org.example.Enums;
 
 public enum Gun {
+     PAZARTESI,  SALI,  CARSAMBA,  PERSEMBE,  CUMA,  CUMARTESI,  PAZAR
 }
--- src/main/java/org/example/Lists/JavaArrayList.java
+++ src/main/java/org/example/Lists/JavaArrayList.java
@@ -1,4 +1,19 @@
 package org.example.Lists;
 
+import java.util.ArrayList;
+
 public class JavaArrayList {
+
+    public void arrayListFunc(){
+        ArrayList<String> cities= new ArrayList<String>();
+        cities.add("İstanbul");
+        cities.add("Ankara");
+        cities.add("İzmir");
+        cities.set(0,"İstanbul");
+
+        for (String city : cities)
+        {
+            System.out.println(city);
+        }
+    }
 }
